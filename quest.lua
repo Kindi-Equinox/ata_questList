ab09203d62265c6f55ae397899c4ffe9ceb9b9af
Kindi-Equinox <91266152+Kindi-Equinox@users.noreply.github.com>
2023-02-26 11:17:48 +0800

    Create quest.lua

---
+++ quest.lua
@@ -0,0 +1,46 @@
+return {
+    Alas = {
+        {
+            id = "Alas:Obtain Stuff",
+            title = "Obtain Stuff",
+            description = "Get this and that and return to this and that",
+            quest = function()
+
+            end,
+        },
+        {
+            id = "Alas:Kill someone",
+            title = "Kill someone",
+            description = "Kill the damn bandit",
+            quest = function()
+
+            end,
+        }
+    },
+    Andalen = {
+        {
+            id = "Andalen:Collect money",
+            title = "Collect money",
+            description = "Collect money from various people",
+            quest = function()
+
+            end,
+        },
+        {
+            id = "Andalen:Retrieve the golden fleece",
+            title = "Retrieve the golden fleece",
+            description = "Help hercules get his fleece back",
+            quest = function()
+
+            end,
+        },
+        {
+            id = "Andalen:MAKAN BANYAK2",
+            title = "Eat all the foods",
+            description = "Reward yourself with earthly delights",
+            quest = function()
+
+            end,
+        }
+    }
+}
